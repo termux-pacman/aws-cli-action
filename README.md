6a705ba82430fa39da9c8110447dc5efd6eba0d5
Ivan Max <60917834+Maxython@users.noreply.github.com>
2022-06-26 23:58:40 +0300

    aws-cli

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # aws-cli-action
+This repository is for installing and configuring aws-cli on github actions.  
+
+Files:
+ - `setup.sh` - Installing and configuring aws-cli.
+ - `func.sh` - it contains functions for convenient work with s3 or packages in pacman format.
---
+++ setup.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/bash
+
+set -e
+
+info() {
+	echo "==> $1"
+}
+
+if [[ -z $1 || -z $2 || -z $3 ]]; then
+	echo "Error: not enough information to connect"
+	exit 1
+fi
+
+info "Install aws cli"
+sudo apt-get install awscli
+
+info "Set aws cli"
+if [ ! -d ~/.aws ]; then
+	mkdir ~/.aws
+fi
+
+if [ ! -f ~/.aws/credentials ]; then
+	{
+	 	echo "[default]"
+		echo "aws_access_key_id = $1"
+		echo "aws_secret_access_key = $2"
+	} > ~/.aws/credentials
+fi
+
+if [ ! -f ~/.aws/config ]; then
+	{
+		echo "[default]"
+		echo "region = $3"
+	} > ~/.aws/config
+fi
